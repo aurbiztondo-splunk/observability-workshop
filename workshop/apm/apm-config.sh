7f1c2b0f4ed07e21db9a5faa7f57d20f4bc01dbe
Achim Staebler <astaebler@splunk.com>
2023-09-25 11:08:49 +0200

    bump up frontend loadgen replicas

--- workshop/apm/apm-config.sh
+++ workshop/apm/apm-config.sh
@@ -5,8 +5,8 @@ while getopts "r" option; do
    r)  norum="False";;
    \?) #For invalid option
       echo "For RUM you have to use: [-r]"
-      exit -1  
-  esac    
+      exit -1
+  esac
 done
 if [ ${norum} == "True" ];
 then
@@ -16,7 +16,7 @@ then
 else
   echo "Adding RUM_TOKEN to deployment"
   if [ -z ${REALM+x} ]; then echo "REALM is unset. Please export REALM=YOUR_REALM"; fi
-  if [ -z ${RUM_TOKEN+x} ]; then echo "RUM_TOKEN is unset. Please export RUM_TOKEN=YOUR_RUM_TOKEN"; fi    
+  if [ -z ${RUM_TOKEN+x} ]; then echo "RUM_TOKEN is unset. Please export RUM_TOKEN=YOUR_RUM_TOKEN"; fi
 fi
-if [ -z ${INSTANCE+x} ]; then echo "INSTANCE is unset. Please export INSTANCE=YOUR_HOST_NAME"; fi 
+if [ -z ${INSTANCE+x} ]; then echo "INSTANCE is unset. Please export INSTANCE=YOUR_HOST_NAME"; fi
 envsubst '${REALM},${RUM_TOKEN},${INSTANCE}' < deployment-RUM-org.yaml > deployment.yaml
