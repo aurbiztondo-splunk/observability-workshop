e3d8a69e70b1f81f4f340a25afab7c8187318f57
Achim Staebler <astaebler@splunk.com>
2023-09-26 13:25:02 +0200

    change name of ssh config

--- workshop/aws/ec2/main.tf
+++ workshop/aws/ec2/main.tf
@@ -326,7 +326,7 @@ resource "signalfx_detector" "k8s_rogue_node_detector" {
 }
 
 locals {
-  ssh_priv_key = "o11y-workshop-${var.slug}.key"
+  ssh_priv_key = "ssh-${var.slug}.key"
 }
 
 resource "local_sensitive_file" "ssh_priv_key" {
@@ -338,7 +338,7 @@ resource "local_sensitive_file" "ssh_priv_key" {
 }
 
 resource "local_file" "ssh_client_config" {
-  filename        = "o11y-workshop-${var.slug}.conf"
+  filename        = "ssh-${var.slug}.conf"
   file_permission = "600"
   content = templatefile("${path.module}/templates/ssh_client_config.tpl",
     {
